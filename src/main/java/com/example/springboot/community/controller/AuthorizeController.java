fd8e63a3bda849da1ab3d9e614e36db1bd1d902d
hzq123 <504899272@qq.com>
2019-08-02 13:54:11 +0800

    add sesson and cookie

--- src/main/java/com/example/springboot/community/controller/AuthorizeController.java
+++ src/main/java/com/example/springboot/community/controller/AuthorizeController.java
@@ -2,6 +2,7 @@ package com.example.springboot.community.controller;
 
 
 import com.example.springboot.community.dto.AccessTokenDTO;
+import com.example.springboot.community.dto.GithubUser;
 import com.example.springboot.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -25,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(client);
@@ -33,6 +37,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
         githubProvider.getAccessToken(new AccessTokenDTO());
-        return "index";
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        if(user != null)
+        {
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+
+        }else{
+            return "redirect:/";
+        }
+
+
     }
 }
